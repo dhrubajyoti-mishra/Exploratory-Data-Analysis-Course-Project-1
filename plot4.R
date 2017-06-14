06e0292f3a4a718ce4c067b94b6f9039c939b60e
Dhrubajyoti Mishra <dhrubajyoti.mishra@gmail.com>
2017-06-14 11:48:09 +0530

    Added code comment

--- plot4.R
+++ plot4.R
@@ -1,8 +1,13 @@
+## Set data file path and name
 dataFile <- "./household_power_consumption.txt"
+## Read the data file
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+## Subset the data only for the dates of interest
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
+## Convert the date time to make it useful
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+## Convert the data to numeric value
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
 voltage <- as.numeric(subSetData$Voltage)
@@ -11,9 +16,11 @@ subMetering2 <- as.numeric(subSetData$Sub_metering_2)
 subMetering3 <- as.numeric(subSetData$Sub_metering_3)
 
 
+## Define png area
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2)) 
 
+## Ploting the graph
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
 
 plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
